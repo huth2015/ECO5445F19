f736be093a0c2578f65ad18845ec5723cde5c362
Lee Morin <Lealand.Morin@ucf.edu>
2019-08-29 15:34:05 -0400

    Uploaded assignment 1

---
+++ assignment_01/hello_world.R
@@ -0,0 +1,10 @@
+# hello_world.R
+# Prints a message to screen. 
+
+# Set node to default. 
+node_num <- 0
+
+# Print message.
+print(sprintf('Hello world, from node number %d.', node_num))
+
+# End
